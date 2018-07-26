6dd6f5c050dd72e955d4c7d096e6f89d221a8a44
Krista <heykristarae@gmail.com>
2018-07-26 15:03:46 -0500

    Added theme credits customization

--- lib/structure/footer.php
+++ lib/structure/footer.php
@@ -9,3 +9,21 @@
  * @license GPL
  */
 namespace KristaRae\Starter;
+
+add_filter( 'genesis_footer_creds_text', __NAMESPACE__ . '\customize_site_credits' );
+/**
+ * Customizes the theme credits in the site footer
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function customize_site_credits () {
+
+	$content = '<div class="creds">' . get_bloginfo( "name" ) . ' &copy; ';
+	$content .= date('Y');
+	$content .= ' | Site By <a href="https://kristarae.co/">Krista Rae</a>';
+	$content .= '</div>';
+
+	echo $content;
+}
