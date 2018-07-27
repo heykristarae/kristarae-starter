8954a34c3d1e1fbab4c773b96a5865f8b23892ee
Krista <heykristarae@gmail.com>
2018-07-27 15:38:40 -0500

    Added footer widget header

--- lib/structure/footer.php
+++ lib/structure/footer.php
@@ -10,6 +10,42 @@
  */
 namespace KristaRae\Starter;
 
+add_action ( 'widgets_init', __NAMESPACE__ . '\register_footer_widgets' );
+/**
+ * Register footer widget areas
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function register_footer_widgets() {
+
+	genesis_register_sidebar( array(
+		'id' => 'footer-widget-header',
+		'name' => __( 'Footer Widget Header', CHILD_TEXT_DOMAIN ),
+		'description' => __( 'This appears above the footer', CHILD_TEXT_DOMAIN ),
+	) );
+
+}
+
+// Position footer widget header
+add_action ( 'genesis_before_footer', __NAMESPACE__ . '\position_above_footer_widgets', 7 );
+/**
+ * Position widget area that appears above footer
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function position_above_footer_widgets ()  {
+
+	genesis_widget_area( 'footer-widget-header', array(
+		'before' => '<div class="footer-widget-header">',
+		'after'  => '</div>',
+	) );
+
+}
+
 add_filter( 'genesis_footer_creds_text', __NAMESPACE__ . '\customize_site_credits' );
 /**
  * Customizes the theme credits in the site footer
