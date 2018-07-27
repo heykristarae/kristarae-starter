ba64af4397e4993d94bfd55cbbd2e4037ea4d29e
Krista <heykristarae@gmail.com>
2018-07-27 16:04:26 -0500

    Mobile menu fix

--- lib/functions/load-assets.php
+++ lib/functions/load-assets.php
@@ -22,8 +22,6 @@ function enqueue_assets() {
 
 	wp_enqueue_style( CHILD_TEXT_DOMAIN . '-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'dashicons' );
-//	wp_enqueue_script( CHILD_TEXT_DOMAIN . '-responsive-menu', CHILD_ASSETS_DIR . 'js/responsive-menus.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
-//	wp_enqueue_script( CHILD_TEXT_DOMAIN . '-responsive-menu', CHILD_URL . '/assets/js/responsive-menus.js', array( 'jquery' ), CHILD_THEME_VERSION, true );
 
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 	wp_enqueue_script(
@@ -42,13 +40,8 @@ function enqueue_assets() {
 		true
 	);
 
-	$localized_script_args = array(
-		'mainMenu' => __( 'Menu', CHILD_TEXT_DOMAIN ),
-		'subMenu'  => __( 'Menu', CHILD_TEXT_DOMAIN ),
-	);
-
 	wp_enqueue_style( 'dashicons' );
 
-	wp_localize_script( CHILD_TEXT_DOMAIN . '-responsive-menu', 'developersL10n', $localized_script_args );
+	wp_localize_script( CHILD_TEXT_DOMAIN . '-responsive-menus', 'genesis_responsive_menu', define_responsive_menu_settings() );
 
 }
